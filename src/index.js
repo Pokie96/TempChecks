5f1fb30c5a7c9bc065d198898ecf764f453ef1c2
adam <19adamchristie96@gmail.com>
2024-03-12 17:57:33 +0000

    Page should now be able to search for cities entered by the user and provide basic information back, displayed on the user interface

--- src/index.js
+++ src/index.js
@@ -5,42 +5,92 @@ import "./style.css";
     as a JavaScript object.
 */
 async function getWeather(location){
-    try{
         const response = await fetch(
             `https://api.weatherapi.com/v1/current.json?key=19be7690d2fb42ce924102105241802&q=${location}`
         );
         const weatherData = await response.json();
-        return weatherData;
-    }
-    catch(error){
-        console.log(error);
-    }
+        if(!weatherData.location){
+            throw new Error("No location found!")
+        }
+        return weatherData; 
 }
 
 /*
     getUserInput returns the user's input from the text input box.
 */
 function getUserInput(){
-    const textInput = document.querySelector("#locationInput");
+    const textInput = document.querySelector("#input");
     const userInput = textInput.value;
     return userInput;
 }
 
 /*
-
+    submitInput calls the Weather API with the user's input as the argument.
+    getWeather is only called if a value has been entered by the user.
 */
 async function submitInput(){
     if(getUserInput() != ""){
-        const data = await getWeather(getUserInput())
-        console.log(data);
+        const data = await getWeather(getUserInput()) 
+        return data;
     } else{
         alert("A location must be entered first!")
     }
 }
 
-const inputForm = document.querySelector(".inputForm");
+/*
+    renderPage renders the new layout of the page once a submission of 
+    the form is made.
+*/
+function renderPage(){
+    //Change existing elements:
+    const contentContainer = document.querySelector("#contentContainer");
+    const pageTitle = document.querySelector("#pageTitle");
+    const inputForm = document.querySelector("#inputForm");
+    const input = document.querySelector("#input");
+    const searchButton = document.querySelector("#searchButton");
+
+    //Change layout of the page:
+    contentContainer.className = "renderedContentContainer";
+    pageTitle.className = "renderedPageTitle";
+    inputForm.className = "renderedInputForm";
+    input.className = "renderedInput";
+    searchButton.className = "renderedSearchButton";
+
+    //Create new elements for the page:
+    const locationName = document.createElement('h3');
+    const locationTemp = document.createElement('p');
+
+    //Add class names to the new elements:
+    locationName.className = "locationName";
+    locationTemp.className = "locationTemp";
+
+    //Append the new elements to the page:
+    contentContainer.appendChild(locationName);
+    contentContainer.appendChild(locationTemp);
+}
+
+function fillPageData(data){
+    const locationName = document.querySelector('.locationName');
+    const locationTemp = document.querySelector('.locationTemp');
+
+    locationName.innerHTML = data.location.name;
+    locationTemp.innerHTML = `${data.current.temp_c}°C`;
+}
+
+const inputForm = document.querySelector(`#inputForm`);
+
+async function pageAction(){
+    await submitInput()
+    .then((resolved) => {
+        console.log(resolved);
+        renderPage();
+        fillPageData(resolved);
+    },(rejected) => {
+        console.log(rejected);
+    })
+}
 
 inputForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    submitInput();
+    pageAction();
 });
